2f4988801d9564ec37db85173348f34391a3e798
wet-cat <fuckingessepi@proton.me>
2026-02-07 13:16:01 +1000

    Create queue.go

---
+++ internal/nfqueue/queue.go
@@ -0,0 +1,26 @@
+package nfqueue
+
+import (
+	"errors"
+
+	nfq "github.com/AkihiroSuda/go-netfilter-queue"
+	"sentinel/internal/engine"
+	"sentinel/internal/logger"
+)
+
+func Run() error {
+	q := nfq.NewNFQueue(0, 200, nfq.NF_DEFAULT_PACKET_SIZE)
+	if q == nil {
+		return errors.New("nfqueue init failed (are you root?)")
+	}
+	defer q.Close()
+
+	logger.Info("nfqueue active")
+
+	for pkt := range q.GetPackets() {
+		verdict := engine.AnalyzePacket(pkt.Data)
+		pkt.SetVerdict(verdict)
+	}
+
+	return nil
+}
